f483a123f2bfc1d6e892554200e41222d4c7fc7c
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-02-16 23:42:50 +0000

    jajaja ehv að gerast

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,33 @@
 -- Útfæra schema
+DROP TABLE IF EXISTS signup;
+DROP TABLE IF EXISTS events;
+
+
+CREATE TABLE IF NOT EXISTS events(
+  id serial PRIMARY KEY,
+  name varchar(64) not NULL,
+  slug varchar(64) not NULL,
+  description varchar(400),
+  created timestamp with time zone not NULL DEFAULT CURRENT_TIMESTAMP,
+  updated timestamp with time zone not NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS signup(
+  id serial PRIMARY KEY,
+  name varchar(64) not NULL,
+  description varchar(400),
+  event integer REFERENCES events(id) not NULL,
+  created timestamp with time zone not NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial primary key,
+  username varchar(64) NOT NULL,
+  password varchar(256) NOT NULL
+);
+
+-- TODO: CHECK password hash
+-- Lykilorð: "123"
+INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,52 @@
+import { readFile } from 'fs/promises';
+import * as faker from 'faker'
+
+import { end, query } from './lib/db.js';
+
+const schemaFile = './sql/schema.sql'
+
+function createFakeEvents(n) {
+  // TODO setja upp gagnagrun + gögn
+  const fake = [];
+
+  while (fake.length < n) {
+    const timeNow = new Date();
+    // TODO fix slug
+    fake.push({
+      name: faker.name.findName(),
+      slug: 'slug',
+      description: faker.Lorem.sentence(),
+      created: timeNow,
+      updated: timeNow
+    })
+  }
+  return fake;
+
+}
+
+function createFakeUsers(n) {
+  // TODO setja upp gagnagrun + gögn
+}
+
+function createFakeSignUps(n) {
+  // TODO setja upp gagnagrun + gögn
+}
 
 async function create() {
   // TODO setja upp gagnagrun + gögn
+  const data = await readFile(schemaFile);
+
+  try {
+    await query(data.toString('utf-8'));
+    console.info('Schema created');
+  } catch (e) {
+    console.error('Error creating schema', e);
+  }
+
+  // TODO add fake data
+  // see: https://github.com/vefforritun/vef2-2021-v3-synilausn/blob/main/src/setup.js
+
+  await end();
 }
 
 create().catch((err) => {
